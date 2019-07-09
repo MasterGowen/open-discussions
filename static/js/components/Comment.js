60d8bc9ea6dda2327e5a94f527c7d0f3717708de
Alice Pote <alice.writes.wrongs@gmail.com>
2019-07-09 10:54:31 -0400

    going to refactor it to be self-contained

--- static/js/components/Comment.js
+++ static/js/components/Comment.js
@@ -1,12 +1,11 @@
 // @flow
-import React from "react"
+import React, { useState } from "react"
 import R from "ramda"
 import moment from "moment"
 import { Link } from "react-router-dom"
 
 import ReportCount from "./ReportCount"
 import Card from "./Card"
-import SpinnerButton from "./SpinnerButton"
 import CommentForm from "../components/CommentForm"
 import CommentVoteForm from "./CommentVoteForm"
 import CommentRemovalForm from "./CommentRemovalForm"
@@ -26,11 +25,15 @@ import type {
   MoreCommentsInTree,
   Post
 } from "../flow/discussionTypes"
-import type { FormsState } from "../flow/formTypes"
 import type { CommentRemoveFunc } from "./CommentRemovalForm"
 import type { CommentVoteFunc } from "./CommentVoteForm"
 
-function Comment() {
+function Comment(props) {
+  const { commentPermalink, post, comment } = this.props
+
+  const [editing, setEditing] = useState(false)
+  const [replying, setReplying] = useState(false)
+
   return (
     <div className={`comment ${comment.removed ? "removed" : ""}`}>
       <Card>
@@ -61,12 +64,12 @@ function Comment() {
             </span>
           </div>
           <div className="row text">
-            {editing.has(comment.id) && post ? (
+            {editing && post ? (
               <CommentForm
                 comment={comment}
                 post={post}
                 closeReply={() => {
-                  this.removeCommentFromState(EDITING, comment)
+                  setEditing(false)
                 }}
                 editing
                 autoFocus
@@ -75,13 +78,13 @@ function Comment() {
               renderTextContent(comment)
             )}
           </div>
-          {replying.has(comment.id) && !atMaxDepth && post ? (
+          {replying && post ? (
             <div>
               <CommentForm
                 post={post}
                 comment={comment}
                 closeReply={() => {
-                  this.removeCommentFromState(REPLYING, comment)
+                  setReplying(false)
                 }}
                 autoFocus
               />
@@ -99,4 +102,144 @@ function Comment() {
   )
 }
 
-function CommentActions() {}
+function CommentActions(props) {
+  const {
+    upvote,
+    downvote,
+    approve,
+    remove,
+    deleteComment,
+    isPrivateChannel,
+    isModerator,
+    reportComment,
+    commentPermalink,
+    moderationUI,
+    ignoreCommentReports,
+    curriedDropdownMenufunc,
+    dropdownMenus,
+    useSearchPageUI,
+    comment
+  } = this.props
+  const { showDropdown, hideDropdown } = curriedDropdownMenufunc(
+    commentDropdownKey(comment)
+  )
+  const {
+    showDropdown: showShareMenu,
+    hideDropdown: hideShareMenu
+  } = curriedDropdownMenufunc(commentShareKey(comment))
+  const commentMenuOpen = dropdownMenus.has(commentDropdownKey(comment))
+  const commentShareOpen = dropdownMenus.has(commentShareKey(comment))
+
+  return (
+    <div className="row comment-actions">
+      {upvote && downvote ? (
+        <CommentVoteForm
+          comment={comment}
+          upvote={upvote}
+          downvote={downvote}
+        />
+      ) : null}
+      {atMaxDepth ||
+      moderationUI ||
+      comment.deleted ||
+      useSearchPageUI ? null : (
+          <ReplyButton
+            beginEditing={e => {
+              e.preventDefault()
+              this.addCommentToState(REPLYING, comment)
+            }}
+          />
+        )}
+      {useSearchPageUI ? null : (
+        <div className="share-button-wrapper">
+          <div
+            className="comment-action-button share-button"
+            onClick={showShareMenu}
+          >
+            share
+          </div>
+          {commentShareOpen ? (
+            <SharePopup
+              url={absolutizeURL(commentPermalink(comment.id))}
+              closePopup={hideShareMenu}
+              hideSocialButtons={isPrivateChannel}
+            />
+          ) : null}
+        </div>
+      )}
+      {!userIsAnonymous() && !useSearchPageUI ? (
+        <div>
+          <i className="material-icons more_vert" onClick={showDropdown}>
+            more_vert
+          </i>
+          {commentMenuOpen ? (
+            <DropdownMenu
+              closeMenu={hideDropdown}
+              className="post-comment-dropdown"
+            >
+              {userIsAnonymous() ? null : this.renderFollowButton(comment)}
+              {SETTINGS.username === comment.author_id && !moderationUI ? (
+                <li>
+                  <div
+                    className="comment-action-button edit-button"
+                    onClick={e => {
+                      e.preventDefault()
+                      this.addCommentToState(EDITING, comment)
+                    }}
+                  >
+                    <a href="#">Edit</a>
+                  </div>
+                </li>
+              ) : null}
+              {SETTINGS.username === comment.author_id && deleteComment ? (
+                <li>
+                  <div
+                    className="comment-action-button delete-button"
+                    onClick={preventDefaultAndInvoke(() =>
+                      deleteComment(comment)
+                    )}
+                  >
+                    <a href="#">Delete</a>
+                  </div>
+                </li>
+              ) : null}
+              {comment.num_reports && ignoreCommentReports ? (
+                <li>
+                  <div
+                    className="comment-action-button ignore-button"
+                    onClick={preventDefaultAndInvoke(() =>
+                      ignoreCommentReports(comment)
+                    )}
+                  >
+                    <a href="#">Ignore reports</a>
+                  </div>
+                </li>
+              ) : null}
+              <li>
+                <CommentRemovalForm
+                  comment={comment}
+                  remove={remove}
+                  approve={approve}
+                  isModerator={isModerator}
+                />
+              </li>
+              {moderationUI || userIsAnonymous() || !reportComment ? null : (
+                <li>
+                  <div
+                    className="comment-action-button report-button"
+                    onClick={preventDefaultAndInvoke(() =>
+                      reportComment(comment)
+                    )}
+                  >
+                    <a href="#">Report</a>
+                  </div>
+                </li>
+              )}
+            </DropdownMenu>
+          ) : null}
+        </div>
+      ) : null}
+      <ReportCount count={comment.num_reports} />
+    </div>
+  )
+}
