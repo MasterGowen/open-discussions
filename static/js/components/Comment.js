bf4280791c9105027eb52efb005d6269cbfefcf1
Alice Pote <alice.writes.wrongs@gmail.com>
2019-09-10 11:50:59 -0400

    wow!

--- static/js/components/Comment.js
+++ static/js/components/Comment.js
@@ -28,11 +28,31 @@ import type {
 import type { CommentRemoveFunc } from "./CommentRemovalForm"
 import type { CommentVoteFunc } from "./CommentVoteForm"
 
-function Comment(props) {
-  const { commentPermalink, post, comment } = this.props
+export default function Comment(props) {
+  const {
+    commentPermalink,
+    post,
+    comment,
+    upvote,
+    downvote,
+    approve,
+    remove,
+    deleteComment,
+    isPrivateChannel,
+    isModerator,
+    reportComment,
+    moderationUI,
+    ignoreCommentReports,
+    curriedDropdownMenufunc,
+    dropdownMenus,
+    useSearchPageUI,
+    atMaxDepth
+  } = props
 
   const [editing, setEditing] = useState(false)
   const [replying, setReplying] = useState(false)
+  const [commentMenuOpen, setCommentMenuOpen] = useState(false)
+  const [commentShareOpen, setCommentShareOpen] = useState(false)
 
   return (
     <div className={`comment ${comment.removed ? "removed" : ""}`}>
@@ -90,159 +110,127 @@ function Comment(props) {
               />
             </div>
           ) : null}
-          <CommentActions 
-            comment={comment}
-            atMaxDepth={atMaxDepth}
-          />
+          <div className="row comment-actions">
+            {upvote && downvote ? (
+              <CommentVoteForm
+                comment={comment}
+                upvote={upvote}
+                downvote={downvote}
+              />
+            ) : null}
+            {atMaxDepth ||
+            moderationUI ||
+            comment.deleted ||
+            useSearchPageUI ? null : (
+                <ReplyButton
+                  beginEditing={e => {
+                    e.preventDefault()
+                    setReplying(true)
+                  }}
+                />
+              )}
+            {useSearchPageUI ? null : (
+              <div className="share-button-wrapper">
+                <div
+                  className="comment-action-button share-button"
+                  onClick={() => setCommentShareOpen(true)}
+                >
+                  share
+                </div>
+                {commentShareOpen ? (
+                  <SharePopup
+                    url={absolutizeURL(commentPermalink(comment.id))}
+                    closePopup={() => setCommentShareOpen(false)}
+                    hideSocialButtons={isPrivateChannel}
+                  />
+                ) : null}
+              </div>
+            )}
+            {!userIsAnonymous() && !useSearchPageUI ? (
+              <div>
+                <i
+                  className="material-icons more_vert"
+                  onClick={() => setCommentMenuOpen(true)}
+                >
+                  more_vert
+                </i>
+                {commentMenuOpen ? (
+                  <DropdownMenu
+                    closeMenu={() => setCommentMenuOpen(false)}
+                    className="post-comment-dropdown"
+                  >
+                    {userIsAnonymous()
+                      ? null
+                      : this.renderFollowButton(comment)}
+                    {SETTINGS.username === comment.author_id &&
+                    !moderationUI ? (
+                        <li>
+                          <div
+                            className="comment-action-button edit-button"
+                            onClick={e => {
+                              e.preventDefault()
+                              setEditing(true)
+                            }}
+                          >
+                            <a href="#">Edit</a>
+                          </div>
+                        </li>
+                      ) : null}
+                    {SETTINGS.username === comment.author_id &&
+                    deleteComment ? (
+                        <li>
+                          <div
+                            className="comment-action-button delete-button"
+                            onClick={preventDefaultAndInvoke(() =>
+                              deleteComment(comment)
+                            )}
+                          >
+                            <a href="#">Delete</a>
+                          </div>
+                        </li>
+                      ) : null}
+                    {comment.num_reports && ignoreCommentReports ? (
+                      <li>
+                        <div
+                          className="comment-action-button ignore-button"
+                          onClick={preventDefaultAndInvoke(() =>
+                            ignoreCommentReports(comment)
+                          )}
+                        >
+                          <a href="#">Ignore reports</a>
+                        </div>
+                      </li>
+                    ) : null}
+                    <li>
+                      <CommentRemovalForm
+                        comment={comment}
+                        remove={remove}
+                        approve={approve}
+                        isModerator={isModerator}
+                      />
+                    </li>
+                    {moderationUI ||
+                    userIsAnonymous() ||
+                    !reportComment ? null : (
+                        <li>
+                          <div
+                            className="comment-action-button report-button"
+                            onClick={preventDefaultAndInvoke(() =>
+                              reportComment(comment)
+                            )}
+                          >
+                            <a href="#">Report</a>
+                          </div>
+                        </li>
+                      )}
+                  </DropdownMenu>
+                ) : null}
+              </div>
+            ) : null}
+            <ReportCount count={comment.num_reports} />
+          </div>
         </div>
       </Card>
-      {atMaxDepth ? null : (
-        <div className="replies">
-          {R.map(renderGenericComment, comment.replies || [])}
-        </div>
-      )}
-    </div>
-  )
-}
-
-function CommentActionsInner(props) {
-  const {
-    upvote,
-    downvote,
-    approve,
-    remove,
-    deleteComment,
-    isPrivateChannel,
-    isModerator,
-    reportComment,
-    commentPermalink,
-    moderationUI,
-    ignoreCommentReports,
-    curriedDropdownMenufunc,
-    dropdownMenus,
-    useSearchPageUI,
-    comment
-  } = this.props
-  const { showDropdown, hideDropdown } = curriedDropdownMenufunc(
-    commentDropdownKey(comment)
-  )
-  const {
-    showDropdown: showShareMenu,
-    hideDropdown: hideShareMenu
-  } = curriedDropdownMenufunc(commentShareKey(comment))
-  const commentMenuOpen = dropdownMenus.has(commentDropdownKey(comment))
-  const commentShareOpen = dropdownMenus.has(commentShareKey(comment))
-
-  return (
-    <div className="row comment-actions">
-      {upvote && downvote ? (
-        <CommentVoteForm
-          comment={comment}
-          upvote={upvote}
-          downvote={downvote}
-        />
-      ) : null}
-      {atMaxDepth ||
-      moderationUI ||
-      comment.deleted ||
-      useSearchPageUI ? null : (
-          <ReplyButton
-            beginEditing={e => {
-              e.preventDefault()
-              this.addCommentToState(REPLYING, comment)
-            }}
-          />
-        )}
-      {useSearchPageUI ? null : (
-        <div className="share-button-wrapper">
-          <div
-            className="comment-action-button share-button"
-            onClick={showShareMenu}
-          >
-            share
-          </div>
-          {commentShareOpen ? (
-            <SharePopup
-              url={absolutizeURL(commentPermalink(comment.id))}
-              closePopup={hideShareMenu}
-              hideSocialButtons={isPrivateChannel}
-            />
-          ) : null}
-        </div>
-      )}
-      {!userIsAnonymous() && !useSearchPageUI ? (
-        <div>
-          <i className="material-icons more_vert" onClick={showDropdown}>
-            more_vert
-          </i>
-          {commentMenuOpen ? (
-            <DropdownMenu
-              closeMenu={hideDropdown}
-              className="post-comment-dropdown"
-            >
-              {userIsAnonymous() ? null : this.renderFollowButton(comment)}
-              {SETTINGS.username === comment.author_id && !moderationUI ? (
-                <li>
-                  <div
-                    className="comment-action-button edit-button"
-                    onClick={e => {
-                      e.preventDefault()
-                      this.addCommentToState(EDITING, comment)
-                    }}
-                  >
-                    <a href="#">Edit</a>
-                  </div>
-                </li>
-              ) : null}
-              {SETTINGS.username === comment.author_id && deleteComment ? (
-                <li>
-                  <div
-                    className="comment-action-button delete-button"
-                    onClick={preventDefaultAndInvoke(() =>
-                      deleteComment(comment)
-                    )}
-                  >
-                    <a href="#">Delete</a>
-                  </div>
-                </li>
-              ) : null}
-              {comment.num_reports && ignoreCommentReports ? (
-                <li>
-                  <div
-                    className="comment-action-button ignore-button"
-                    onClick={preventDefaultAndInvoke(() =>
-                      ignoreCommentReports(comment)
-                    )}
-                  >
-                    <a href="#">Ignore reports</a>
-                  </div>
-                </li>
-              ) : null}
-              <li>
-                <CommentRemovalForm
-                  comment={comment}
-                  remove={remove}
-                  approve={approve}
-                  isModerator={isModerator}
-                />
-              </li>
-              {moderationUI || userIsAnonymous() || !reportComment ? null : (
-                <li>
-                  <div
-                    className="comment-action-button report-button"
-                    onClick={preventDefaultAndInvoke(() =>
-                      reportComment(comment)
-                    )}
-                  >
-                    <a href="#">Report</a>
-                  </div>
-                </li>
-              )}
-            </DropdownMenu>
-          ) : null}
-        </div>
-      ) : null}
-      <ReportCount count={comment.num_reports} />
     </div>
   )
 }
@@ -294,14 +282,8 @@ const mapStateToProps = (state, ownProps) => {
     errored:
       anyErrorExcept404([posts, channels]) ||
       anyErrorExcept404or410([comments]),
-    subscribedChannels:         getSubscribedChannels(state),
-    commentInFlight:            comments.processing,
-    postDeleteDialogVisible:    ui.dialogs.has(DELETE_POST_DIALOG),
-    commentDeleteDialogVisible: ui.dialogs.has(DELETE_COMMENT_DIALOG),
-    postReportDialogVisible:    ui.dialogs.has(REPORT_POST_DIALOG),
-    commentReportDialogVisible: ui.dialogs.has(REPORT_COMMENT_DIALOG),
-    embedly:                    embedlyResponse
+    subscribedChannels: getSubscribedChannels(state),
+    commentInFlight:    comments.processing,
+    embedly:            embedlyResponse
   }
 }
-
-
--- static/js/components/CommentTree.js
+++ static/js/components/CommentTree.js
@@ -16,6 +16,7 @@ import ProfileImage, { PROFILE_IMAGE_MICRO } from "./ProfileImage"
 import DropdownMenu from "../components/DropdownMenu"
 import ReplyButton from "./ReplyButton"
 import ShareTooltip from "./ShareTooltip"
+import Comment from './Comment'
 
 import { preventDefaultAndInvoke, userIsAnonymous } from "../lib/util"
 import { makeProfile } from "../lib/profile"
@@ -254,73 +255,14 @@ export default class CommentTree extends React.Component<Props, State> {
     const atMaxDepth = depth + 1 >= SETTINGS.max_comment_depth
 
     return (
-      <div
-        className={`comment ${comment.removed ? "removed" : ""}`}
-        key={`comment-${comment.id}`}
-      >
-        <Card>
-          <Link to={profileURL(comment.author_id)}>
-            <ProfileImage
-              profile={makeProfile({
-                name:                comment.author_name,
-                username:            SETTINGS.username,
-                profile_image_small: comment.profile_image
-              })}
-              imageSize={PROFILE_IMAGE_MICRO}
-            />
-          </Link>
-          <div className="comment-contents">
-            <div className="author-info">
-              <Link to={profileURL(comment.author_id)}>
-                <span className="author-name">{comment.author_name}</span>
-              </Link>
-              <Link to={commentPermalink(comment.id)}>
-                <span className="authored-date">
-                  {moment(comment.created).fromNow()}
-                </span>
-              </Link>
-              <span className="removed-note">
-                {comment.removed ? (
-                  <span>[comment removed by moderator]</span>
-                ) : null}
-              </span>
-            </div>
-            <div className="row text">
-              {editing.has(comment.id) && post ? (
-                <CommentForm
-                  comment={comment}
-                  post={post}
-                  closeReply={() => {
-                    this.removeCommentFromState(EDITING, comment)
-                  }}
-                  editing
-                  autoFocus
-                />
-              ) : (
-                renderTextContent(comment)
-              )}
-            </div>
-            {replying.has(comment.id) && !atMaxDepth && post ? (
-              <div>
-                <CommentForm
-                  post={post}
-                  comment={comment}
-                  closeReply={() => {
-                    this.removeCommentFromState(REPLYING, comment)
-                  }}
-                  autoFocus
-                />
-              </div>
-            ) : null}
-            {this.renderCommentActions(comment, atMaxDepth)}
-          </div>
-        </Card>
+      <React.Fragment>
+        <Comment comment={comment} atMaxDepth={atMaxDepth} commentPermalink={commentPermalink} />
         {atMaxDepth ? null : (
           <div className="replies">
             {R.map(renderGenericComment, comment.replies || [])}
           </div>
         )}
-      </div>
+      </React.Fragment>
     )
   }
 
